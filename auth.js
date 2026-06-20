e86b5882ef8bb3b588e812d6d7feb135c843cdb8
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-06-19 22:10:18 -0400

    fix

--- auth.js
+++ auth.js
@@ -1,3 +1,61 @@
+// // TOKEN GLOBAL
+// let token = localStorage.getItem("token") || "";
+
+// // REGISTRO
+// async function register() {
+//   const username = document.getElementById("user").value.trim();
+//   const password = document.getElementById("pass").value.trim();
+
+//   const res = await fetch('/register', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ username, password })
+//   });
+
+//   const data = await res.json();
+//   alert(data.ok ? "Registrado" : data.error);
+
+//   if (data.ok) window.location.href = "login.html";
+// }
+
+// // LOGIN
+// async function login() {
+//   const username = document.getElementById("user").value.trim();
+//   const password = document.getElementById("pass").value.trim();
+
+//   const res = await fetch('/login', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ username, password })
+//   });
+
+//   const data = await res.json();
+
+//   if (!data.ok) {
+//     alert(data.error);
+//     return;
+//   }
+
+//   localStorage.setItem("token", data.token);
+//   window.location.href = "tareas.html";
+// }
+
+// // LOGOUT
+// async function logout() {
+//   const token = localStorage.getItem("token");
+
+//   await fetch('/logout', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ token })
+//   });
+
+//   localStorage.removeItem("token");
+//   window.location.href = "login.html";
+// }
+// URL de tu backend en Render
+const API = "https://backend-lm93.onrender.com";
+
 // TOKEN GLOBAL
 let token = localStorage.getItem("token") || "";
 
@@ -6,7 +64,7 @@ async function register() {
   const username = document.getElementById("user").value.trim();
   const password = document.getElementById("pass").value.trim();
 
-  const res = await fetch('/register', {
+  const res = await fetch(`${API}/register`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password })
@@ -23,7 +81,7 @@ async function login() {
   const username = document.getElementById("user").value.trim();
   const password = document.getElementById("pass").value.trim();
 
-  const res = await fetch('/login', {
+  const res = await fetch(`${API}/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password })
@@ -44,7 +102,7 @@ async function login() {
 async function logout() {
   const token = localStorage.getItem("token");
 
-  await fetch('/logout', {
+  await fetch(`${API}/logout`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ token })
